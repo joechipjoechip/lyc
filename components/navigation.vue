f763d08ca71ccf64e4a1b8b6b2e082d37628f299
Lionel <joechipjoechip@gmail.com>
2024-02-14 17:36:40 +0100

    improve nav

--- components/navigation.vue
+++ components/navigation.vue
@@ -2,35 +2,49 @@
 </script>
 
 <template>
-    <nav>
-        <NuxtLink to="/">home</NuxtLink>
-        <NuxtLink to="/contact">contact</NuxtLink>
+    <nav class="nav-wrapper">
+        <div class="nav-inner">
+            <NuxtLink to="/">home</NuxtLink>
+            <NuxtLink to="/contact">contact</NuxtLink>
+        </div>
     </nav>
 </template>
 
 <style lang="scss" scoped>
-    nav {
-        // border: solid 2px red;
-        height: $layoutNavHeightDesktop;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        text-align: right;
+    .nav {
 
-        background-color: var(--color-main-30);
-        backdrop-filter: blur(8px);
-
-        * {
-            color: var(--color-main-90);
-            margin: 0 2rem;
-
-            &:first-of-type {
-                margin-left: 0;
-            }
-
-            &:last-of-type {
-                margin-right: 0;
-            }
+        &-wrapper {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            height: $layoutNavHeightDesktop;
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: flex-end;
+            align-items: center;
+            
+            background-color: var(--color-main-30);
+            backdrop-filter: blur(8px);
         }
+
+        &-inner {
+            border: solid 1px red;
+            margin-right: 2rem;
+
+            * {
+                color: var(--color-main-90);
+                margin: 0 2rem;
+    
+                &:first-of-type {
+                    margin-left: 0;
+                }
+    
+                &:last-of-type {
+                    margin-right: 0;
+                }
+            }
+
+        }
+
     }
 </style>
