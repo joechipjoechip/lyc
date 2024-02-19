0af2bdfb6f395e0f6adf90f0e30c78a359937c2d
Lionel <joechipjoechip@gmail.com>
2024-02-19 17:10:03 +0100

    micro title component

---
+++ components/micro/title.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+    isGradient: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <h2 class="title"
+        :class="{ isGradient }"
+    >{{ text }}</h2>
+</template>
+
+<style lang="scss" scoped>
+.title {
+    font-family: "Mada Bold";
+    font-size: var(--font-size-big-plus);
+    text-transform: uppercase;
+
+    &.isGradient {
+        background: var(--gradient-one);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+}
+</style>
