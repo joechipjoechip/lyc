1624f82e86f02147a5a846c7e9a3b0056007f785
Lionel <joechipjoechip@gmail.com>
2024-02-22 18:28:56 +0100

    restablish isMobile

--- stores/main.js
+++ stores/main.js
@@ -4,8 +4,7 @@ export const useMainStore = defineStore('storedMain', {
 
 		state: () => ({
 			gyroIsAllowed: false,
-            // isMobile: window.matchMedia("(max-width: 667px), (pointer: coarse)").matches,
-            isMobile: false,
+            isMobile: window.matchMedia("(max-width: 667px), (pointer: coarse)").matches,
             isIOS: navigator.userAgent.includes("iPhone") || navigator.userAgent.includes("iPad")
 		}),
 
