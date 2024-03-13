2074bacfbbfb7994f222221bcd03bbe4d8d96faf
Lionel <joechipjoechip@gmail.com>
2024-03-13 14:48:15 +0100

    test tuning

--- components/micro/Hud.vue
+++ components/micro/Hud.vue
@@ -78,20 +78,20 @@ function handleGyro(event){
     const animatedObject = { 
         tx: translateX.value ? parseInt(translateX.value.replace("rem", "")) : 0, 
         ty: translateY.value ? parseInt(translateY.value.replace("rem", "")) : 0,
-        rx: rotateX.value,
-        ry: rotateY.value,
+        rx: rotateX.value ? rotateX.value : 0,
+        ry: rotateY.value ? rotateY.value : 0,
     }
 
     gsap.to(animatedObject, {
         tx: x * ratioHudTransformOnGyro.tx,
         ty: y * ratioHudTransformOnGyro.ty,
-        rx: x * ratioHudTransformOnGyro.rx,
-        ry: y * ratioHudTransformOnGyro.ry,
+        rx: parseInt(x * ratioHudTransformOnGyro.rx),
+        ry: parseInt(y * ratioHudTransformOnGyro.ry),
         duration: 0.2,
         ease: "linear",
         onUpdate: () => {
             translateX.value = animatedObject.tx + "rem"
-            translateY.value = animatedObject.ty + "rem"
+            translateY.value = animatedObject.ty + 1.5 + "rem"
             rotateX.value = animatedObject.rx
             rotateY.value = animatedObject.ry
         }
