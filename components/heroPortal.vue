563833089600fab404a8a4b1092be0682a53cac6
Lionel <joechipjoechip@gmail.com>
2024-03-14 22:03:53 +0100

    tuning portal

--- components/heroPortal.vue
+++ components/heroPortal.vue
@@ -122,8 +122,8 @@ function handleGyro(event){
     // console.log("gyro y : ", y)
 
     gsap.to(animatedObject, {
-        x: clamp(x / 1.75, -1.75, 1.75),
-        y: clamp((y * 35 + 300) / 9, -1, 9),
+        x: clamp(x / 1.5, -1.5, 1.5),
+        y: clamp((y * 35 + 300) / 9, -0.25, 9),
         duration: 0.2,
         ease: "linear",
         onUpdate: () => {
