b4ea783d4833a6bfea77848a8ab93b5370bcb5a6
Lionel <joechipjoechip@gmail.com>
2024-03-14 16:35:39 +0100

    ajout signin page iframe

---
+++ pages/signin.vue
@@ -0,0 +1,54 @@
+<script setup>
+useHead({
+    script: {
+        src: "https://tally.so/widgets/embed.js",
+        async: true
+    }
+})
+
+let tallyLogo = null
+
+onMounted(() => {
+    setTimeout(() => {
+
+        document.querySelectorAll('iframe').forEach( item =>
+            tallyLogo = item.contentWindow.document.body.querySelector('.tally-powered')
+        )
+
+        console.log("tally : ", tallyLogo)
+        if( tallyLogo ){
+            tallyLogo.style.display = "none"
+        }
+    }, 4000)
+})
+</script>
+
+<template>
+    <div class="signin-wrapper">
+        <div class="signin-inner">
+            <iframe data-tally-src="https://tally.so/embed/mZNEvV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="120" frameborder="0" marginheight="0" marginwidth="0" title="Emails"></iframe>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.signin {
+    &-wrapper {
+        position: relative;
+        width: 100%;
+        max-width: $layoutMaxWidthDesktop;
+        margin: 0 auto;
+        min-height: 60vh;
+
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    &-inner {
+        max-width: 30rem;
+        margin: 0 auto;
+    }
+}
+</style>
