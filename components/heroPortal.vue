125a28b398318ce0b0952820bb33609996ba9db5
Lionel <joechipjoechip@gmail.com>
2024-03-14 15:40:26 +0100

    sdsd

--- components/heroPortal.vue
+++ components/heroPortal.vue
@@ -121,7 +121,7 @@ function handleGyro(event){
 
     gsap.to(animatedObject, {
         x: clamp(x / 4, -0.85, 0.85),
-        y: clamp(y, -1, 100),
+        y: y+300,
         duration: 0.2,
         ease: "linear",
         onUpdate: () => {
