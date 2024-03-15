5866c8e2f1999ffeffc279feef52c27b9fc40e38
Lionel <joechipjoechip@gmail.com>
2024-03-15 15:13:02 +0100

    patch svg shit

--- components/icons/play.vue
+++ components/icons/play.vue
@@ -12,5 +12,5 @@ const props = defineProps({
 })
 </script>
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" :width="width || '12rem'" :height="height || '12rem'" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2M9.5 16.5v-9l7 4.5z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" :width="width || '12em'" :height="height || '12em'" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2M9.5 16.5v-9l7 4.5z"></path></svg>
 </template>
