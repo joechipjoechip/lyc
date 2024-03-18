25862ce2db3e354b70a1141c381032b4cc2c5e2b
Lionel <joechipjoechip@gmail.com>
2024-03-18 15:21:03 +0100

    add favicon and meta and og

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,8 +39,40 @@ export default defineNuxtConfig({
 		head: {
 			htmlAttrs: {
 				class: 'dark'
-			}
+			},
+
+			link: [
+				{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+				{ rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+				{ rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+				{ rel: "manifest", href: "/site.webmanifest" },
+				{ rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
+			  ],
+			meta: [
+				{ property: "og:title", content: "LOOT YOUR CITY | Crypto Treasure Hunt | Augmented Reality Mobile Game" },
+				{ property: "og:description", content: "Loot Your City web3 play-to-earn is the next biggest crypto treasure hunt on the blockchain Solana, playable soon on your mobile all over the World. Get your Key NFT now in early access and get in Looters Community." },
+				{ property: "og:url", content: "https://www.lootyourcity.com" },
+				{ property: "og:image", content: "https://www.lootyourcity.com/images/visuals/strip-loot.jpg" },
+		
+				{ name: "msapplication-TileColor", content: "#da532c" },
+				{ name: "theme-color", content: "#ffffff" }
+			],
+
+			twitterCard: 'summary_large_image',
+			twitterSite: '@lootyourcity',
+
+			title: "LOOT YOUR CITY | Crypto Treasure Hunt | Augmented Reality Mobile Game" ,
+			description: "Loot Your City web3 play-to-earn is the next biggest crypto treasure hunt on the blockchain Solana, playable soon on your mobile all over the World. Get your Key NFT now in early access and get in Looters Community.",
 		}
+
+
+	// 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	// <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	// <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	// <link rel="manifest" href="/site.webmanifest">
+	// <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	// <meta name="msapplication-TileColor" content="#da532c">
+	// <meta name="theme-color" content="#ffffff">
 
 	},	
 
